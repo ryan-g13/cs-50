c9742ee28e2602ca4adc85326cd30e7646956955
Ryan Groesch <ryan.groesch@gmail.com>
2018-07-12 18:30:52 -0700

    alphanumeric functionality included Caesar cypher done

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -12,35 +12,27 @@ int main(int argc, string argv[]) {
     int k = atoi(argv[1]);
     string plainText = get_string("plaintext: ");
     int length = strlen(plainText);
-    char cypherText[4] = "";
-    printf("Cypher %s", cypherText);
+    char cypherText[60] = "";
     for (int i = 0; i < length; i++) {
-        // if (isalpha(plainText[i])) {
-        //     continue;
-         if (isupper(plainText[i])) {
+        if (!isalpha(plainText[i])) {
+            cypherText[i] = plainText[i];
+        } else if (isupper(plainText[i])) {
             int c = (int)plainText[i] - 65;
             int converted = (c + k) % 26;
             converted = converted + 65;
-            printf("post add %i", converted);
             char temp = (char)converted;
-            printf("Temp char: %c", temp);
-            eprintf("Cypher @ %c", cypherText[i]);
             cypherText[i] = temp;
-            printf("A String? %s", cypherText);
             // ascii starts at 65 for caps, A = 0 using mod 26 to wrap things around and ensure still upper.
         }
         else {
             int c = (int)plainText[i] - 97;
             int converted = (c + k) % 26;
             converted = converted + 97;
-            printf("post add %i", converted);
             char temp = (char)converted;
-            printf("Temp char: %c", temp);
-            eprintf("Cypher @ %c", cypherText[i]);
             cypherText[i] = temp;
             // ascii starts at 97 for lower, a = 0 using mod 26 to wrap things around and ensure still lower.
             }
         }
-    eprintf("cyphertext: %s\n", cypherText);
+    printf("cyphertext: %s\n", cypherText);
     return 0;
 }
